f0fd624952ead06b0d7a024088d2da21b94e69ae
James Newman <jamesmnewman1@gmail.com>
2015-09-28 21:55:48 -0700

    user create method

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 # syntax change to a structure like github.com/jnewman12/rspec-example
+# comments are the newer expect syntax (which is the one recommended),
+# and the uncommented ones are the should syntax which was added in rails_helper.rb
 describe Api::V1::UsersController do
 	before (:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
 
@@ -17,6 +19,42 @@ describe Api::V1::UsersController do
 		end
 
 		it { should respond_with 200 }
+	end
+
+	describe "POST #create" do
+	 # this is what i was missing in github.com/jnewman12/respec-example, minus the json 
+		before(:each) do
+	        @user_attributes = FactoryGirl.attributes_for :user 
+	        post :create, { user: @user_attributes }, format: :json
+		end
+
+		it "renders the json attributes for the user just created" do 
+			user_response = JSON.parse(response.body, symbolize_names: true)
+			# expect(user_response[:email]).to eql @user_attributes[:email]
+			user_response[:email].should eql @user_attributes[:email]
+		end
+		it { should respond_with 201 }
+
+		context "when it is not created" do 
+			before(:each) do
+		       @invalid_user_attributes = { password: "12345678",
+		                                    password_confirmation: "12345678" }
+		       post :create, { user: @invalid_user_attributes }, format: :json
+			end
+
+			it "renders a JSON error" do 
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				# expect(user_response).to have_key(:errors)
+				user_response.should have_key(:errors)
+			end
+
+			it "renders an error why the user was not created" do 
+				user_response = JSON.parse(response.body, symbolize_names: true) 
+				# expect(user_response[:errors][:email]).to include "can't be blank"
+				user_response[:errors][:email].should include "can't be blank"
+			end
+			it { should respond_with 422 }
+		end
 	end
 
 end
