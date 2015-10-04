198f455d4e678397727974c4ed1db7b3e2e5e232
James Newman <jamesmnewman1@gmail.com>
2015-10-03 21:07:29 -0700

    sessions create + specs passing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,34 +18,21 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.include Devise::TestHelpers, type: :controller
+
   # to use the outdated should syntax instead of expect
   config.expect_with(:rspec) { |c| c.syntax = :should }
+
   # adding json_response helper
   config.include RequestHelper, :type => :controller
 
+  # call our RequestHelper module method before each controller run
   config.before(:each, type: :controller) do
       include_default_accept_headers
   end
+
 end
