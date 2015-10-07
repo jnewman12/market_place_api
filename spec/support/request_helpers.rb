20a5fce220e4034d6526c5c282be490fb1a5241b
James Newman <jamesmnewman1@gmail.com>
2015-10-06 21:40:04 -0700

    create action + tests

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -4,21 +4,23 @@ module RequestHelper
 	end
 
 	def api_header(version = 1)
-		request.headers["Accept"] = "application/vnd.marketplace.v#{version}"
+	  request.headers["Accept"] = "application/vnd.marketplace.v#{version}"
 	end
 
 	def api_response_format(format = Mime::JSON)
-		request.headers['Accept'] = "#{request.headers['Accept']},#{format}"
-		request.headers['Content-Type'] = format.to_s
+	  request.headers['Accept'] = "#{request.headers['Accept']},#{format}"
+	  request.headers['Content-Type'] = format.to_s
 	end
 
 	# this is so each time we need to call current_user in the specs, we can
+	# and this is an attribute we set on our current_user function to make sure it's only
+	# a real current user
 	def api_authorization_header(token)
 	  request.headers['Authorization'] = token 
 	end
 
 	def include_default_accept_headers
-		api_header
-		api_response_format
+	  api_header
+	  api_response_format
 	end
 end
