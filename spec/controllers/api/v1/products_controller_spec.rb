12e362b677af4ec58934fa3d7691bc86ba2ce36d
James Newman <jamesmnewman1@gmail.com>
2015-10-17 23:18:29 -0700

    slowly...still not working and have no clue why?????????

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 #require 'spec_helper'
 
 describe Api::V1::ProductsController, :controller => true do
-	# describe "#GET show" do 
-	# 	before(:each) do 
-	# 		@product = FactoryGirl.create :product
-	# 		get :show, id: @product.id
-	# 	end
+	describe "#GET show" do 
+		before(:each) do 
+			@product = FactoryGirl.create :product
+			get :show, id: @product.id
+		end
 
-	# 	it "returns the product information of a certain id" do 
-	# 		product_response = json_response[:product]
-	# 		# expect(product_response[:title]).to eql @product.title
-	# 		product_response[:title].should eql @product.title
-	# 	end
+		it "returns the product information of a certain id" do 
+			product_response = json_response[:product]
+			# expect(product_response[:title]).to eql @product.title
+			product_response[:title].should eql @product.title
+		end
 
-	# 	it "has a user as an embedded object" do 
-	# 		product_response = json_response[:product]
-	# 		# expect(product_response[:user][:email]).to eql @product.user.email
-	# 		product_response[:user][:email].should eql @product.user.email
+		it "has a user as an embedded object" do 
+			product_response = json_response[:product]
+			# expect(product_response[:user][:email]).to eql @product.user.email
+			product_response[:user][:email].should eql @product.user.email
 
-	# 	end
-	# 	it { should respond_with 200 }
-	# end
+		end
+		it { should respond_with 200 }
+	end
 
 	describe "#GET index" do 
 		# added collection matchers (have(4)) gem, was core now a gem
