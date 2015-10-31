83b1c9883e39b834c1528d3c801af65d8bc8f4ca
James Newman <jamesmnewman1@gmail.com>
2015-10-30 20:31:54 -0700

    so meta

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -47,7 +47,13 @@ describe Api::V1::ProductsController, :controller => true do
 			  product_response[:user].should be_present
 			end
 		end
-		it { should respond_with 200 }
+		it { expect(json_response).to have_key(:meta) }
+        it { expect(json_response[:meta]).to have_key(:pagination) }
+        it { expect(json_response[:meta][:pagination]).to have_key(:per_page) }
+        it { expect(json_response[:meta][:pagination]).to have_key(:total_pages) }
+        it { expect(json_response[:meta][:pagination]).to have_key(:total_objects) }
+
+        it { should respond_with 200 }
 	end
 	
 	describe "POST #create" do
