26d2996a4224fef6a502cdbaeb354cd548842e61
James Newman <jamesmnewman1@gmail.com>
2015-10-31 16:18:05 -0700

    green order specs

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -15,6 +15,12 @@ describe Api::V1::OrdersController, type: :controller do
 		  order_response.should have(4).items
 		end
 
+		it { json_response.should have_key(:meta) }
+		it { json_response[:meta].should have_key(:pagination) }
+		it { json_response[:meta][:pagination].should have_key(:per_page) }
+		it { json_response[:meta][:pagination].should have_key(:total_pages) }
+		it { json_response[:meta][:pagination].should have_key(:total_objects) }
+
 		it { should respond_with 200 }
 	end
 
