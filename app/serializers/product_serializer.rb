cbe23fcb1c4adffc97327344d33fb2d3e71a5312
James Newman <jamesmnewman1@gmail.com>
2015-10-31 16:44:52 -0700

    final commit....ready for the front end

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,6 +2,7 @@ class ProductSerializer < ActiveModel::Serializer
  	attributes :id, :title, :published
  	attributes :user
 
+ 	# this reduces response time about 90%
  	def cache_key
 	  [object, scope]
 	end
